bd032d2126f2f963e0b0df25718461f2ee1e1e7e
ccdale <chris.charles.allison+git@gmail.com>
2024-03-02 10:10:23 +0000

    card object module

---
+++ ccacards/card.py
@@ -0,0 +1,65 @@
+import sys
+
+from ccaerrors import errorExit, errorNotify, errorRaise
+import ccalogging
+
+log = ccalogging.log
+
+
+class Card:
+    """Representation of a Playing Card."""
+
+    valueNames = [
+        "Ace",
+        "Two",
+        "Three",
+        "Four",
+        "Five",
+        "Six",
+        "Seven",
+        "Eight",
+        "Nine",
+        "Ten",
+        "Jack",
+        "Queen",
+        "King",
+    ]
+
+    suitNames = ["Spades", "Hearts", "Diamonds", "Clubs"]
+
+    def __init__(self, index, facedown=False):
+        """Initialises the Card
+
+        args:
+            index: int 0 - 52
+                0 is a placeholder for 'no card' i.e. blank space
+                1 == Ace of Spades
+                13 == King of Spades
+                14 == Ace of Hearts
+                26 == King of Hearts
+                27 == Ace of Diamonds
+                39 == King of Diamonds
+                40 == Ace of Clubs
+                52 == King of Clubs
+        """
+        try:
+            self.facedown = facedown
+            self.index = index
+            self.value = (self.index - 1) % 13
+            self.valuename = self.valueNames[self.value]
+            self.suitindex = (self.index - 1) // 13
+            self.suit = self.suitNames[self.suitindex]
+        except Exception as e:
+            errorRaise(sys.exc_info()[2], e)
+
+    def __str__(self):
+        try:
+            return "Face Down" if self.facedown else f"{self.valuename} of {self.suit}"
+        except Exception as e:
+            errorRaise(sys.exc_info()[2], e)
+
+    def __repr__(self):
+        try:
+            return f"Card({self.index})"
+        except Exception as e:
+            errorRaise(sys.exc_info()[2], e)
---
+++ tests/test_card.py
@@ -0,0 +1,15 @@
+from ccacards.card import Card
+
+
+def test_Card():
+    c = Card(14)  # Ace of Hearts
+    assert c.value == 0
+    assert c.valuename == "Ace"
+    assert str(c) == "Ace of Hearts"
+
+
+def test_Card_maths():
+    c = Card(46)  # 7 of clubs
+    assert c.value == 6
+    assert c.valuename == "Seven"
+    assert str(c) == "Seven of Clubs"
