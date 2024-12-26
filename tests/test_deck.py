9ebbbedc23d631792dd380c65a860950ffe8d11a
ccdale <chris.charles.allison+git@gmail.com>
2024-12-26 11:02:17 +0000

    playing card images

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -15,3 +15,11 @@ def test_Deck_noaces():
     xc = d.deal(13)
     assert str(xc[-1]) == "King of Diamonds"
     assert str(xc[0]) == "King of Clubs"
+
+
+def test_Deck_deall():
+    d = Deck()
+    c = d.deall(5)
+    assert len(d) == 47
+    assert str(c[-1]) == "Nine of Clubs"
+    assert str(c[0]) == "King of Clubs"
